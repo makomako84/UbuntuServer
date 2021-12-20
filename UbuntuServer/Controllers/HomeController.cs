f76812ac88310c484df068314ee365f5eadb22d9
Artur Galimsky <artur.galimsky@gmail.com>
2021-12-20 17:01:02 +0500

    works

--- UbuntuServer/Controllers/HomeController.cs
+++ UbuntuServer/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace UbuntuServer.Controllers
 {
@@ -8,10 +13,29 @@ namespace UbuntuServer.Controllers
     [Produces("application/json")]
     public class HomeController : Controller
     {
-        [HttpGet("get")]
-        public IActionResult Get()
+        [HttpPost("import")]
+        public IActionResult Import(IFormFile file)
         {
-            return CreatedAtAction(nameof(Get), new {value = 4});
+            return CreatedAtAction(nameof(Import), ImportFile(file));
+        }
+
+        private List<string> ImportFile(IFormFile file)
+        {
+            using var stream = new MemoryStream();
+            file.CopyTo(stream);
+            stream.Position = 0;
+
+            using var workBook = new XLWorkbook(stream);
+            var workSheet = workBook.Worksheets.FirstOrDefault();
+
+            IEnumerable<IXLRow> usedRows = workSheet.Rows(2, workSheet.RowsUsed().Count());            
+            List<string> collection = new List<string>();
+            collection.Add(usedRows.FirstOrDefault().Cell(1).CachedValue.ToString());
+            collection.Add(usedRows.FirstOrDefault().Cell(2).CachedValue.ToString());
+            collection.Add(usedRows.FirstOrDefault().Cell(3).CachedValue.ToString());
+
+            return collection;
+
         }
     }
 }
